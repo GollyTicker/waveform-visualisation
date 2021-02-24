00d413a2ce4b78eebd986aee3a42c1bcca8ab94a
GollyTicker <golly.ticker@gmail.com>
2021-02-25 00:03:02 +0100

    long debugging... but it works. video cannot handle transparency :-)

--- converter.js
+++ converter.js
@@ -1,6 +1,7 @@
-const VIDEO_FRAMES_PER_SECOND = 10;
+const VIDEO_FRAMES_PER_SECOND = 15;
 const SECOND = 1;
 const SECOND_TO_MILLIS = 1000;
+const FPS_AS_MILLIS = SECOND / VIDEO_FRAMES_PER_SECOND * SECOND_TO_MILLIS;
 
 let wave = new Wave();
 
@@ -12,41 +13,42 @@ options = {type: "cubes", stroke: 2, colors: ["#fdd"]};
 
 options = {type: "dualbars", stroke: 2, colors: ["#fdd"]};
 
-let encoder = {};
-
 let audio = () => document.getElementById("audio");
 let canvas = () => document.getElementById("canvas");
 let video = () => document.getElementById("video");
 
-let addFrame = () => {
-  encoder.add(canvas());
-}
+let encoder = new Whammy.Video(VIDEO_FRAMES_PER_SECOND);
+console.log("Initialised encoder.")
 
-let renderVideo = () => {
-  encoder.compile(false,(output) => {
-      let URL = (window.webkitURL || window.URL);
-      const url = URL.createObjectURL(output);
-      video().src = url;
-    }
-  )
+let addFrame = () => {
+  const ctx = document.getElementById('canvas').getContext('2d');
+	ctx.fillStyle = 'white';
+  ctx.fillRect(0,0,400,90);
+  ctx.fillStyle = 'black';
+  ctx.fillRect(40,40,80,80);
+  encoder.add(document.getElementById('canvas').getContext('2d'));
 }
 
 let timer = 0;
 
 let convertHandler = () => {
   if(audio().paused) {
+    console.log("Start. Call every ",FPS_AS_MILLIS," ms");
     audio().play();
-    timer = setInterval(addFrame, SECOND / VIDEO_FRAMES_PER_SECOND * SECOND_TO_MILLIS);
+    timer = setInterval(addFrame, FPS_AS_MILLIS);
   }
   else {
+    console.log("Stop.");
     audio().pause();
     clearInterval(timer);
-    renderVideo();
+      console.log("Compiling...");
+      var output = encoder.compile();
+      console.log("Found data: ", output.toString());
+      document.getElementById("video").src = webkitURL.createObjectURL(output);
   }
 }
 
 window.onload = () => {
   wave.fromElement("audio","canvas",options);
   document.getElementById("convertButton").onclick = convertHandler;
-  encoder = new Whammy.Video(VIDEO_FRAMES_PER_SECOND);
 }
