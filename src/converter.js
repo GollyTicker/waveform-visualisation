77db9dba9d4884b05330eb45ed6f32ba70d64bcf
GollyTicker <golly.ticker@gmail.com>
2021-04-13 23:56:42 +0200

    Added logic for full audio conversion. Refactoring.

--- src/converter.js
+++ src/converter.js
@@ -26,11 +26,19 @@ options = {
   colors: ["white"]
 };
 
-const encoder = new Whammy.Video(VIDEO_FRAMES_PER_SECOND);
+let encoder = undefined;
+
+let resetEncoder = () => {
+  encoder = new Whammy.Video(VIDEO_FRAMES_PER_SECOND);
+};
 
 let audio = () => document.getElementById("audio");
 let canvas = () => document.getElementById("canvas");
 let video = () => document.getElementById("video");
+let recordingIndicator = () => document.getElementById("recordingIndicator");
+
+const isRecordingText = "RECORDING";
+const notRecordingText = "not recording";
 
 let addFrame = () => {
   encoder.add(canvas().getContext("2d"));
@@ -41,20 +49,55 @@ let renderVideo = () => {
   let URL = (window.webkitURL || window.URL);
   const url = URL.createObjectURL(output);
   video().src = url;
+  resetEncoder();
 }
 
 let timer = 0;
 
-let convertHandler = () => {
+let toogleAudioPlayback = () => {
   if (audio().paused) {
     audio().play();
-    timer = setInterval(addFrame, SECOND / VIDEO_FRAMES_PER_SECOND * SECOND_TO_MILLIS);
   } else {
     audio().pause();
-    clearInterval(timer);
-    renderVideo();
+  }
+  return audio().paused;
+}
+
+let restartRecording = () => {
+  resetEncoder();
+  timer = setInterval(addFrame, SECOND / VIDEO_FRAMES_PER_SECOND * SECOND_TO_MILLIS);
+  recordingIndicator().classList.add("active");
+  recordingIndicator().innerText = isRecordingText;
+}
+
+let finishRecording = () => {
+  clearInterval(timer);
+  renderVideo();
+  recordingIndicator().classList.remove("active");
+  recordingIndicator().innerText = notRecordingText;
+};
+
+let conversionStepHandler = () => {
+  const wasPaused = toogleAudioPlayback();
+  if (wasPaused) {
+    finishRecording();
+  } else {
+    restartRecording();
   }
 }
+
+let conversionFullHandler = () => {
+  if (!audio().paused) {
+    audio().pause();
+  }
+  audio().currentTime = 0;
+  conversionStepHandler();
+}
+
+let audioEndedHandler = () => {
+  console.log("Audio ended!");
+  finishRecording();
+};
 
 function drawBackgroundCanvas() {
   const ctx = canvas().getContext("2d");
@@ -66,7 +109,10 @@ window.onload = () => {
   document.body.style.backgroundColor = bgColor;
   wave.fromElement("audio", "canvas", options);
   drawBackgroundCanvas();
-  document.getElementById("convertButton").onclick = convertHandler;
+  document.getElementById("convertStepButton").onclick = conversionStepHandler;
+  document.getElementById("convertFullButton").onclick = conversionFullHandler;
+  audio().onended = audioEndedHandler;
+  recordingIndicator().innerText = notRecordingText;
 }
 
 const handleUploadedFile = (file) => {
